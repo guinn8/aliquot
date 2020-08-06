8e619c6d2b38e966016b387a03c644cd2d7f1510
Gavin Guinn <gavinguinn1@gmail.com>
2020-08-06 15:51:11 -0600

    improved and threaded basicEnnum

--- basicEnnum/main.c
+++ basicEnnum/main.c
@@ -2,38 +2,72 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <omp.h>
 
-unsigned long max_bound;
+const unsigned long max_bound = 1000000000;
+const int countMax = 256;
 
 unsigned long wheelDivSum(unsigned long n);
-void recParents(char characArr[max_bound/2]);
+void recParents(char * characArr, unsigned long max_bound);
 
 int main(int argc, char *argv[]){
-    max_bound = atol(argv[1]);
+    remove("preimagesParents.csv");
+
+   
+    unsigned long current_bound = 10000;
+    unsigned long last_bound = 0;
+
+    FILE * fp = fopen("preimagesParents.csv","a");
+    fprintf(fp, "bound, 0");
+    for(int i = 1; i < countMax; i++){
+       fprintf(fp, ",%d", i);
+    }
+    fprintf(fp, "\n");
+    fclose(fp);
+
     char * localCharac = malloc(max_bound / 2);
-    unsigned long s_n ;
-
-    for(unsigned long i = 2; i <= max_bound*2; i+=2){
-        s_n = wheelDivSum(i);
-        printf("i: %lu out of: %lu\n", i, max_bound*2);
-        if(s_n <= max_bound && s_n % 2 == 0 ) {
-            localCharac[(s_n / 2)-1]++;
-        }
-       
+    for(int i = 0; i < max_bound / 2; i++){
+            localCharac[i] = 0;
     }
 
-    for(unsigned long i = 1; i <= max_bound; i+= 2){
-        s_n = wheelDivSum(i*i);
-        printf("i: %lu out of: %lu\n", i, max_bound);
-        if(s_n <= max_bound && s_n % 2 == 0 ) {
-            localCharac[(s_n / 2)-1]++;
+   
+    while(current_bound <= max_bound){
+        
+
+        #pragma omp parallel
+        {
+            #pragma omp section
+            {
+                #pragma omp for 
+                for(unsigned long i = 2; i <= current_bound*2; i+=2){
+                    unsigned long s_n = wheelDivSum(i);
+                    //printf("i: %lu out of: %lu\n", i, max_bound*2);
+                    if(s_n > last_bound && s_n <= current_bound && s_n % 2 == 0 ) {
+                        #pragma omp atomic
+                        localCharac[(s_n / 2)-1]++;
+                    }   
+                }
+            }
+        
+       
+            #pragma omp section
+            {
+                for(unsigned long i = 1; i <= current_bound; i+= 2){
+                    unsigned long s_n = wheelDivSum(i*i);
+                    //printf("i: %lu out of: %lu\n", i, max_bound);
+                    if(s_n > last_bound && s_n <= current_bound && s_n % 2 == 0 ) {
+                        #pragma omp atomic
+                        localCharac[(s_n / 2)-1]++;
+                    }
+                }    
+            }
         }
        
-    }
 
-    recParents(localCharac);
-    
- 
+        recParents(localCharac, current_bound);
+        last_bound = current_bound;
+        current_bound += 10000;
+    }
 }
 
 const int initWheel [3] = {2, 3, 5 };
@@ -91,7 +125,9 @@ unsigned long wheelDivSum(unsigned long n){
     return res -savedN;
 }
 
-void recParents(char characArr[max_bound/2]){
+void recParents(char * characArr, unsigned long max_bound){
+
+    FILE * fp = fopen("preimagesParents.csv","a");
 
     unsigned long accPreimages[256] = {0};
 
@@ -101,8 +137,11 @@ void recParents(char characArr[max_bound/2]){
         accPreimages[characArr[i]]++;
     }
 
-    printf("\nBound = %lu\n", max_bound);
-    for(int j = 0;  j < 8; j++){
-       printf("\n%d count: %lu, density = %f", j, accPreimages[j], (float)accPreimages[j]/max_bound);
+    fprintf(fp,"%lu", max_bound);
+    for(int j = 0;  j < countMax; j++){
+       fprintf(fp, ",%lu",  accPreimages[j] );
     }
+    fprintf(fp, "\n");
+
+    fclose(fp);
 }

4f75e9f556b688efdec7f7d819cb14a275eea6ec
Gavin Guinn <gavinguinn1@gmail.com>
2020-08-20 14:10:51 -0600

    changed most data types to fixed width

--- preCount/main.c
+++ preCount/main.c
@@ -1,31 +1,40 @@
+#define FLINT
+//#define ASSERT //for any production runs ensure assert is not defined
+#define TIMING
+
 #include "properSumDiv.h"
 #include "sieve.h"
 #include <omp.h>
 #include <assert.h> 
-//#include "/home/gavin.guinn/flint-2.6.2/arith.h"
 #include <string.h>
+#include <stdint.h>
+#ifdef FLINT
 #include <flint/arith.h>
-#define TIMING
-#define FLINT
-//#define ASSERT //for any production runs ensure assert is not defined
+#endif
+//#include "/home/gavin.guinn/flint-2.6.2/arith.h"
 
-unsigned long s(unsigned long n);
-unsigned long s_sqInput(unsigned long n);
-int writePreimage(ulong preimage, ulong * imageChunk, int chunkCount, char * characFunc);
-void writeBuffer( ulong * imageChunk, int chunkCount, char * characFunc);
-void tabStats(unsigned char * characArr);
-int chooseNumChunks(ulong max_bound);
 
-unsigned long max_bound;
-unsigned long chunk_size;
-int buffer_size;
-int numChunks ;
+void writeBuffer(uint64_t * imageChunk, uint32_t chunkCount, uint8_t * characFunc);
+void tabStats(uint8_t * characArr);
+uint32_t writePreimage(uint64_t preimage, uint64_t * imageChunk, uint32_t chunkCount, uint8_t * characFunc);
+uint32_t chooseNumChunks(uint64_t max_bound);
+uint64_t s(uint64_t n);
+uint64_t s_sqInput(uint64_t n);
 
-unsigned long upperNumPres;
-unsigned long writtenPreAcc = 0;
+uint32_t buffer_size;
+uint32_t numChunks;
 
-int main(int argc, char *argv[]){
+uint64_t max_bound;
+uint64_t chunk_size;
+uint64_t upperNumPres;
+uint64_t writtenPreAcc = 0;
 
+
+//uints64 are used ensure consistant behaviour about over unsigned wrap
+//We are safe as long as all values are below 18,446,744,073,709,551,615
+//The largest number that can possibly be produced is: max_bound ^ 4/3
+//This means that approx 281474976700000 (2.81x10^14) is the largest safe value for max_bound
+void main(uint32_t argc, uint8_t *argv[]){
 
     double startTime = omp_get_wtime();
 
@@ -56,35 +65,35 @@ int main(int argc, char *argv[]){
 
     //byte array that accumates parent information
     //characFunc[i] holds the number of parents for (i + 1) * 2
-    unsigned char * characFunc = malloc(max_bound/2);
+    uint8_t * characFunc = malloc(max_bound/2);
     memset(characFunc, 0, max_bound/2 );
 
     //These buffers are nessicary for the prime seive
     //Most of this is pulled straight from Anton's implementation
     //so I really dont have a good idea how it work
-    const unsigned long max_prime = (unsigned long) sqrt((double) (2 * max_bound));
-	const unsigned prime_bound = (unsigned long) (1.25506 * (max_prime + 1) / log(max_prime + 1));
-	unsigned int * primes = (unsigned int *) malloc(sizeof(unsigned int) * (prime_bound + 1));
+    const uint64_t max_prime = (uint64_t) sqrt(2 * max_bound);
+	const uint64_t prime_bound = (uint64_t) (1.25506 * (max_prime + 1) / log(max_prime + 1));
+	uint32_t * primes = ( uint32_t *) malloc(sizeof(uint32_t) * (prime_bound + 1));
 	prime_sieve(max_prime, primes);
 
     //These vars handle the odd composite squares
     //these numbers are pushed into a buffer to be processed in the second part of the algor
-    unsigned long compositeBound = (unsigned long)pow(max_bound, .6666666666666);
-    unsigned long * compSquares =  malloc(sizeof(unsigned long) * (compositeBound));
-    unsigned long compSquareCounter = 0;
+    const uint64_t compositeBound = (uint64_t)pow(max_bound, .6666666666666666666);
+    uint64_t * compSquares =  malloc(sizeof(uint64_t) * (compositeBound));
+    uint64_t compSquareCounter = 0;
 
     #pragma omp parallel
     {
         //sigma holds the sum-of-divisors returned by antons sum_of_divisors_odd
-        unsigned long * sigma =  malloc ((chunk_size /2)*sizeof(unsigned long));
+        uint64_t * sigma =  malloc ((chunk_size /2)*sizeof(uint64_t));
 
         //this buffer holds preimages before they are written to disk in a batch
-        unsigned long * imageChunk = malloc (sizeof(unsigned long)* buffer_size);
-        int chunkCount; //counter for the imageChunk buffer
+        uint64_t * imageChunk = malloc (sizeof(uint64_t)* buffer_size);
+        uint32_t chunkCount; //counter for the imageChunk buffer
        
         //splits the problem into chunks for threading
         #pragma omp for schedule(auto) 
-        for(int i = 0; i < numChunks; i++){
+        for(uint32_t i = 0; i < numChunks; i++){
 
             #ifdef TIMING
             double threadStart = omp_get_wtime();
@@ -92,12 +101,12 @@ int main(int argc, char *argv[]){
 
             chunkCount = 0;
 
-            unsigned long  m = (i * chunk_size);//the number currently being processed by algo
+            uint64_t  m = (i * chunk_size);//the number currently being processed by algo
 
             sum_of_divisors_odd(chunk_size, m, sigma, primes);//Antons again, very fast sum of divisors
       
             //runs through the thread specific chunk
-            for(unsigned long j = 0; j < chunk_size / 2; j++){
+            for(uint64_t j = 0; j < chunk_size / 2; j++){
               
                 //sigma(m) = sigma[j] 
                 m = (i * chunk_size + 1) + (2 * j); //cannot remember where tf this offset comes from
@@ -109,10 +118,10 @@ int main(int argc, char *argv[]){
                 //catches evens values of sigma[j] and runs them through recurrance
                 if(!(sigma[j] & 1)){ 
 
-                    unsigned long  t = 3 * sigma[j] - (2 * m);
+                    uint64_t  t = 3 * sigma[j] - (2 * m);
                    
                     #ifdef ASSERT
-                    int pow = 2;
+                    uint32_t pow = 2;
                     #endif
 
                     while (t <= max_bound){
@@ -161,12 +170,12 @@ int main(int argc, char *argv[]){
 
         //Im not sure this nowait is safe but it seems to work
         #pragma omp for nowait
-        for(unsigned long  i = 0; i < compSquareCounter; i++){
+        for(uint64_t  i = 0; i < compSquareCounter; i++){
 
 	        #ifdef FLINT
-            unsigned long s_mSq = s(compSquares[i]*compSquares[i]);
+            uint64_t s_mSq = s(compSquares[i]*compSquares[i]);
 	        #else
-            unsigned long s_mSq = wheelDivSum(compSquares[i]*compSquares[i]);
+            uint64_t s_mSq = wheelDivSum(compSquares[i]*compSquares[i]);
 	        #endif
 
             #ifdef ASSERT
@@ -189,7 +198,7 @@ int main(int argc, char *argv[]){
 
 //writes a preimage to the chunks buffer
 //if the buffer is full it is written to the charac function
-int writePreimage(ulong preimage, ulong * imageChunk, int chunkCount, char * characFunc){
+uint32_t writePreimage(uint64_t preimage, uint64_t * imageChunk, uint32_t chunkCount, uint8_t * characFunc){
 
     imageChunk[chunkCount] = preimage;
     chunkCount++;
@@ -203,13 +212,13 @@ int writePreimage(ulong preimage, ulong * imageChunk, int chunkCount, char * cha
 }
 
 //This function writes a buffer of preimages to the characFile
-void writeBuffer( ulong * imageChunk, int chunkCount, char * characFunc){
+void writeBuffer( uint64_t * imageChunk, uint32_t chunkCount, uint8_t * characFunc){
 
     #ifdef TIMING
     double chunkTime  = omp_get_wtime();
     #endif
 
-    for(int i = 0; i < chunkCount; i++){
+    for(uint32_t i = 0; i < chunkCount; i++){
 
         #ifdef ASSERT
         assert(imageChunk[i] <= max_bound);
@@ -221,26 +230,27 @@ void writeBuffer( ulong * imageChunk, int chunkCount, char * characFunc){
         characFunc[(imageChunk[i]/2)-1]++;
     }
     writtenPreAcc += chunkCount;
+    
     #ifdef TIMING
-    printf("%%%-.2f || %d preimages written in %f seconds\n", (float)writtenPreAcc/upperNumPres ,chunkCount, omp_get_wtime()-chunkTime);
+    printf("%%%-.2f || %d preimages written in %f seconds\n", (float)100 * writtenPreAcc/upperNumPres ,chunkCount, omp_get_wtime()-chunkTime);
     #endif
 }
 
 //This functions processes the characArray and tabulates statisics about k-parent numbers
 //characArr must be max_bound/2 in size
-void tabStats(unsigned char * characArr){
+void tabStats(uint8_t * characArr){
 
     FILE * fp = fopen("counts.csv","a");
 
-    unsigned long accPreimages[256] = {0};
+    uint64_t accPreimages[256] = {0};
 
     accPreimages[0]++; //accounts for 5 which is the
-    for(unsigned long i = 0;  i < max_bound/2; i++){
+    for(uint64_t i = 0;  i < max_bound/2; i++){
         accPreimages[characArr[i]]++;
     }
 
     fprintf(fp,"%lu", max_bound);
-    for(int j = 0;  j < 255; j++){
+    for(uint32_t j = 0;  j < 255; j++){
        fprintf(fp, ",%lu",  accPreimages[j] );
     }
     fprintf(fp, "\n");
@@ -248,7 +258,7 @@ void tabStats(unsigned char * characArr){
     fclose(fp);
 }
 
-unsigned long s(unsigned long n){
+uint64_t s(uint64_t n){
     fmpz_t res, num;
     fmpz_init(res);
     fmpz_init_set_ui(num,n);
@@ -259,7 +269,7 @@ unsigned long s(unsigned long n){
     return fmpz_get_ui(res);
 }
 
-unsigned long s_sqInput(unsigned long n){
+uint64_t s_sqInput(uint64_t n){
     fmpz_t res, num;
     
     fmpz_init(res);
@@ -271,11 +281,14 @@ unsigned long s_sqInput(unsigned long n){
     return fmpz_get_ui(res);
 }
 
-
-int chooseNumChunks(ulong max_bound){
-    int chunks;
-    int root = sqrt(max_bound);
-    for (int i=1; i <= max_bound; i++){
+//When threading in this manner it is nessicary to choose a number of
+//chunks to split the problem into such that each thread can work through the problem
+//a chunk at a time
+//Chooses the first divisor greater than the sqrt of max bound to be the number of chunks
+uint32_t chooseNumChunks(uint64_t max_bound){
+    uint32_t chunks;
+    uint32_t root = sqrt(max_bound);
+    for (uint32_t i=1; i <= max_bound; i++){
         if (max_bound % i==0){
             
             if(i >= root && (max_bound / i) % 2 == 0){
